0af9a2174e01e6eeaec58aa8cece1f55157dd927
JaySant <jaysant15@gmail.com>
2022-11-18 03:33:21 +0000

    Requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,9 @@
+const CategoryModel = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: DataTypes.INTEGER,
+        name: DataTypes.STRING
+    });
+    return Category
+}
+
+module.exports = CategoryModel;
